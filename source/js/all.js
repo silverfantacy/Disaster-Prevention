da7677e4a2cc7b1853498da1313f6b89c6c21519
Zero <silverfantacy@gmail.com>
2017-07-30 01:07:02 +0800

    增加下拉選單

--- source/js/all.js
+++ source/js/all.js
@@ -1,47 +1,6 @@
-const apiUrl = 'https://tcgbusfs.blob.core.windows.net/blobfs/GetDisasterSummary.json'; //將apiUrl設定為常數不再變動。
-
 var app = new Vue({
-    el: '.app',
-    data: {
-        area: '全部',
-        disasterData: null,
-        showinfo: null
-    },
-    created: function () {
-        //執行
-        this.callData()
-
-    },
-    filter: {
-
-    },
-    methods: {
-        filterArea: function (item) {
-            if (this.area == '全部') {
-                return true
-            } else if (item.CaseLocationDistrict == this.area) {
-                return true
-            }
-        },
-        callData: function () {
-            // GET /someUrl
-            this.$http.get(apiUrl).then(response => {
-
-
-                // 獲取災害資訊
-                this.disasterData = response.body.DataSet['diffgr:diffgram'].NewDataSet.CASE_SUMMARY;
-
-                this.showinfo = response.status;
-
-            }, response => {
-                this.showinfo = response.status;
-            });
-        }
-
-
-    }
-
-    /*https://github.com/pagekit/vue-resource*/
-    /*VUE RESOURCE*/
-
+  el:'#app',
+  data: {
+    message : 'Hello World！'
+  }
 })
