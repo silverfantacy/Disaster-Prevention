53a12204892f6ab92974a56ca25ae525cc5b154a
Zero <silverfantacy@gmail.com>
2017-07-30 11:43:44 +0800

    Update 2017-07-30T03:43:39.629Z

--- js/all.js
+++ js/all.js
@@ -6,4 +6,24 @@ var app = new Vue({
     message: 'Hello World！'
   }
 });
+
+var locationData = '';
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'https://tcgbusfs.blob.core.windows.net/blobfs/GetDisasterSummary.json', true);
+xhr.send(null);
+xhr.onload = function () {
+  var str = JSON.parse(xhr.responseText);
+  locationData = str.DataSet['diffgr:diffgram'].NewDataSet.CASE_SUMMARY;
+  //   撈到資料後再執行你要執行的 function
+  renderHTML();
+};
+function renderHTML() {
+  var str = '';
+  var len = locationData.length;
+  console.log(locationData);
+  for (var i = 0; len > i; i++) {
+    str += '<li>' + locationData[i].CaseLocationDistrict + '：' + locationData[i].CaseDescription + '</li>';
+  }
+  document.querySelector('.show').innerHTML = str;
+}
 //# sourceMappingURL=all.js.map
