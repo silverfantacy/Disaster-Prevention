beb9c4a2ba293d043d6e0351e4f310fe30a9c71d
Zero <silverfantacy@users.noreply.github.com>
2017-08-01 16:57:59 +0800

    增加Map的頁面

--- source/js/all.js
+++ source/js/all.js
@@ -13,31 +13,53 @@ var app = new Vue({
   },
   methods: {
     filterArea: function (item) {
+      //篩選區域
       if (this.area == '全部') {
-        return true
+        return true;
       } else if (item.CaseLocationDistrict == this.area) {
-        return true
+        return true;
       }
     },
     callData: function () {
       // GET /someUrl
       this.$http.get(apiUrl).then(response => {
 
-
-        // 獲取災害資訊
+        // 獲取災害資訊，取得資料
         this.disasterData = response.body.DataSet['diffgr:diffgram'].NewDataSet.CASE_SUMMARY;
-
+        initMap(this.disasterData);
         this.showinfo = response.status;
 
       }, response => {
         this.showinfo = response.status;
       });
     }
-
-
   }
-
   /*https://github.com/pagekit/vue-resource*/
   /*VUE RESOURCE*/
-
 })
+
+// Google Map API
+
+function initMap(data) {
+  // 設定中心點座標
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 25.0329636,
+      lng: 121.5654268
+    },
+    zoom: 13
+  });
+  for (i = 0; i < data.length; i++) {
+    var str = {};
+    var place = {};
+
+    place.lat = parseFloat(data[i].Wgs84X);
+    place.lng = parseFloat(data[i].Wgs84Y);
+
+    str.map = map;
+    str.title = data[i].Name;
+    str.position = place;
+    // console.log(place);
+    new google.maps.Marker(str);
+  }
+}
